ecd50f6a1d98b5ac035bfca9cbbe86c449fe8e14
ceianreigh <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-10-10 09:55:28 +0800

    create a menu and loop to keep the program running

--- expense_tracker.py
+++ expense_tracker.py
@@ -2,4 +2,18 @@
 def main():
     expenses = []
     
+    # create a menu
+    choices = {1: 'Add an expense', 
+               2 : 'List all expenses', 
+               3: 'Show total expenses', 
+               4: 'Filter expenses by category', 
+               5: 'Exit'}
+    
+    # create a loop to keep the program running
+    while True:
+        print("Welcome to your Expense Tracker!")
+
+        for key, value in choices.items():
+            print(f'{key}. {value}')
+        break
 main()
